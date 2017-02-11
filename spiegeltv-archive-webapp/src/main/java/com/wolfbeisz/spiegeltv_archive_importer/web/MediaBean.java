4011676e3b9c9fb9e0d5f96abca2cd1bcbeecea4
Philipp <philippwolfbeisz@gmail.com>
2017-02-11 12:10:15 +0100

    fix webapp bug: non-widescreen videos could not be played

--- spiegeltv-archive-webapp/src/main/java/com/wolfbeisz/spiegeltv_archive_importer/web/MediaBean.java
+++ spiegeltv-archive-webapp/src/main/java/com/wolfbeisz/spiegeltv_archive_importer/web/MediaBean.java
@@ -40,4 +40,18 @@ public class MediaBean implements Serializable {
 		MediumDataProvider mediumDataProvider = new MediumDataProvider(em_);
 		dm_ = new MediumDataModel(mediumDataProvider);
 	}
+	
+	public String m3u8StreamUrl(Medium medium) {
+		if (medium.getWide()) {
+			return "http://sptv-vod.dcp.adaptive.level3.net/"+ medium.getUuid() +"_spiegeltv_0500_16x9.m4v.m3u8";
+		}
+		return "http://sptv-vod.dcp.adaptive.level3.net/"+ medium.getUuid() +"_spiegeltv_0500_4x3.m4v.m3u8";
+	}
+	
+	public String rtmpStreamUrl(Medium medium) {
+		if (medium.getWide()) {
+			return "rtmp://s3pwwozuj5hrsa.cloudfront.net/cfx/st/mp4:"+ medium.getUuid() + "_spiegeltv_0500_16x9.m4v";
+		}
+		return "rtmp://s3pwwozuj5hrsa.cloudfront.net/cfx/st/mp4:"+ medium.getUuid() + "_spiegeltv_0500_4x3.m4v";
+	}
 }
